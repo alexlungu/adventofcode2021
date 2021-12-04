7a0384c5ff949517fa7028f4d5ac4bbe4e464ed3
Alex Lungu <a.lungu@hotmail.com>
2021-12-04 13:19:45 +0000

    Day 2 challenge done

---
+++ dayTwoPartOne.php
@@ -0,0 +1,38 @@
+<?php
+require 'vendor/autoload.php';
+
+$filename = './dayTwoInput.txt';
+
+if (!file_exists($filename)) {
+    dd("$filename cannot be found");
+}
+$horizontalPos = $depth = 0;
+if ($handle = fopen($filename, 'rb')) {
+    while (($line = fgets($handle)) !== false) {
+        $line = trim($line);
+        preg_match('/(?\'direction\'forward|down|up)\s(?\'value\'\d+)/', $line, $instructions);
+
+        if (!isset($instructions['direction'], $instructions['value'])) {
+            // Direction of travel or value missing
+            continue;
+        }
+
+        if ($instructions['direction'] === 'forward') {
+            $horizontalPos += (int)$instructions['value'];
+        }
+
+        if ($instructions['direction'] === 'down') {
+            $depth += (int)$instructions['value'];
+        }
+
+        if ($instructions['direction'] === 'up') {
+            $depth -= (int)$instructions['value'];
+        }
+    }
+    fclose($handle);
+}
+dump("Horizontal position $horizontalPos");
+dump("Depth $depth");
+dd($horizontalPos * $depth);
+
+
